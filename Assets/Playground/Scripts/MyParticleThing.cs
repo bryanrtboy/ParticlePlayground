55a5ec170d3f7cacf462cf6878433bc6f6b81e96
bryanrtboy <bryan@bryanleister.com>
2019-02-06 12:46:04 -0700

    Initial commit

---
+++ Assets/Playground/Scripts/MyParticleThing.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MidiJack;
+
+public class MyParticleThing : MonoBehaviour
+{
+    public int m_knobNumber = 9;
+    public ParticleSystem m_particleSystem;
+
+    void Awake()
+    {
+        transform.localScale = Vector3.zero;
+    }
+
+    void Update()
+    {
+        float s = MidiMaster.GetKnob(m_knobNumber);
+        Debug.Log("Value of knob " + m_knobNumber + " is " + s.ToString("#.####"));
+
+        if (m_particleSystem != null && s > 0f)
+        {
+            var main = m_particleSystem.main;
+
+            main.startSize = s * 10f;
+
+        }
+    }
+}
