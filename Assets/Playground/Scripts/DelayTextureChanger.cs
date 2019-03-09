423468c54227b60074d4bc8ec03b26fd2f6df4ea
bryanrtboy <bryan@bryanleister.com>
2019-03-09 14:20:24 -0700

    Added a Don't Clear scene

---
+++ Assets/Playground/Scripts/DelayTextureChanger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Renderer))]
+public class DelayTextureChanger : MonoBehaviour
+{
+    public Texture m_newTexture;
+    public float m_delay;
+
+    Renderer m_renderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_renderer = this.GetComponent<Renderer>();
+        Invoke("Swap", m_delay);
+    }
+
+    void Swap()
+    {
+        Destroy(this.gameObject);
+    }
+}
